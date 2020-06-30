a19de6c7353368372757deea8a6347c78242b278
Nahue <nahuelflores1373@gmail.com>
2020-06-30 16:56:06 -0300

    Se agrega un buscar por celda

--- src/Mapa/Celda.java
+++ src/Mapa/Celda.java
@@ -1,7 +1,5 @@
 package Mapa;
 
-
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
--- src/grafics/ConvertidorDeTexto.java
+++ src/grafics/ConvertidorDeTexto.java
@@ -18,8 +18,30 @@ public class ConvertidorDeTexto {
 		
 	public ConvertidorDeTexto () {
 		caracteres = "abcdefghijklmnopqrstuvwxyz 0123456789.,!?'\"-+=/\\%()<>:;";
-		}
-		
+	}
+
+//--------------------------------------GETTERS--------------------------------------------//
+
+	public String getCaracteres() {
+		return caracteres;
+	}
+
+//--------------------------------------SETTERS--------------------------------------------//
+	
+	public void setCaracteres(String caracteres) {
+		this.caracteres = caracteres;
+	}	
+	
+//--------------------------------------METODOS PROPIOS-------------------------------------//
+	
+/**
+ * Este metodo recorre el String Caracteres para encontrar la posicion del char c y devolver su posicion.
+ * 
+ * @param c es el caracter a buscar en el String.
+ * @return un entero que es la posicion del Sprite correspondiente al caracter.
+ * @throws CaracterNoExisteException esto ocurre cuando el caracter buscado no se encuentra en el string Caracteres.
+ */
+	
 	public int buscar(char c) throws CaracterNoExisteException {
 		int i = 0;
 		Boolean flag = false;
@@ -40,39 +62,43 @@ public class ConvertidorDeTexto {
 		return i;
 		
 	}
+	
+/**
+ * Este metodo recorre el String mensaje y realiza llamadas al metodo buscar para poder convertir el mensaje en un ArrayList con las posiciones de los Sprites de cada caracter. 
+ * 	
+ * @param mensaje es un String a convertir.
+ * @return un ArrayList de int con la misma longitud que el String Mensaje que contiene la posicion de los Sprites para mostrar el menaje requerido.
+ */
 		
-		public ArrayList<Integer> convertirMensaje(String mensaje) {
-		
-			int longMensaje = mensaje.length();
-			ArrayList<Integer> numSprites = new ArrayList<Integer>();
-			int i = 0;
-			
-			
-				while (i<longMensaje) {
-					
-						try {
-							numSprites.add(i,buscar(mensaje.charAt(i)));
-						} catch (CaracterNoExisteException e) {
-							System.out.println("Este caracter "+e.getCaracter()+" no existe entre los Sprites");
-						}
-				
-					
-					i++;
-			}
-				
-			return numSprites;
-		}
-		
-		public void mostrar() {
-			int i = 0;
-			while (i<caracteres.length()) {
-				
-				System.out.println(i+" --> "+caracteres.charAt(i));
+	public ArrayList<Integer> convertirMensaje(String mensaje) {
+		int longMensaje = mensaje.length();
+		ArrayList<Integer> numSprites = new ArrayList<Integer>();
+		int i = 0;
+
+			while (i<longMensaje) {					
+					try {
+						numSprites.add(i,buscar(mensaje.charAt(i)));
+					} catch (CaracterNoExisteException e) {
+						System.out.println("Este caracter "+e.getCaracter()+" no existe entre los Sprites");
+					}
 				i++;
 			}
-		}
-		
+		return numSprites;
 	}
+	
+/**
+ * Muestra cada uno de los Sprites que existen con su posicion en el arreglo.
+ */
+	public void mostrar() {
+		int i = 0;
+		while (i<caracteres.length()) {
+				
+			System.out.println(i+" --> "+caracteres.charAt(i));
+			i++;
+		}
+	}
+		
+}
 
 	
 
