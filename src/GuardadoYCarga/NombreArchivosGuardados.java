b241d9e2cbd40ddbac5190d6569b8f472c8b5354
Nahuel Flores <nahuelflores1373@gmail.com>
2020-07-02 13:24:29 -0300

    Se termino la Documentasao

--- src/GuardadoYCarga/NombreArchivosGuardados.java
+++ src/GuardadoYCarga/NombreArchivosGuardados.java
@@ -3,8 +3,18 @@ package GuardadoYCarga;
 import java.io.File;
 import java.util.ArrayList;
 
+/**
+ * En esta clase se puede leer el directorio y levantar los nombres de las partidas guardadas.
+ * @author Nahue 
+ *
+ */
+
 public class NombreArchivosGuardados {
 
+	/**
+	 * Lee el directorio y pone los nombres de las partidas guardadas en el ArrayList que luego retorna.
+	 * @return Un ArrayList de Strings con los nombres de las partidas guardadas.
+	 */
 	public static ArrayList<String> nombreArchivosGuardados(){
 		
 		File carpetaFile = new File(System.getProperty("user.dir"));
@@ -13,7 +23,7 @@ public class NombreArchivosGuardados {
 		
 		for (int i = 0; i < listadoStrings.length; i++) {
 				
-			if (listadoStrings[i].charAt(0)=='P'||listadoStrings[i].charAt(1)=='G') {
+			if (listadoStrings[i].charAt(0)=='P'& listadoStrings[i].charAt(1)=='G') {
 				partidasGuardadas.add(listadoStrings[i]);
 			}
 		}
