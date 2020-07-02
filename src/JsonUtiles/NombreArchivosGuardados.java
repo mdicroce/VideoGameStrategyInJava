965969617caf8ac074ec17f0d8fc6754e5cd209d
Nahuel Flores <nahuelflores1373@gmail.com>
2020-07-02 02:00:37 -0300

    Arregla3 v2

--- src/JsonUtiles/NombreArchivosGuardados.java
+++ src/JsonUtiles/NombreArchivosGuardados.java
@@ -4,13 +4,19 @@ import java.io.File;
 
 public class NombreArchivosGuardados {
 
-	public static String nombreArchivosGuardados(){
-		File carpetaFile = new File(sCarpAct);
+	public static String[] nombreArchivosGuardados(){
+		File carpetaFile = new File(System.getProperty("user.dir"));
 		String[] listadoStrings = carpetaFile.list();
-		
-		for (int i = 0; i < listadoStrings.length; i++) {
-			System.out.println(listadoStrings[i]);
+		if (listadoStrings==null|| listadoStrings.length == 0) {
+			System.out.println("No Bro");
 		}
+		else {
+			for (int i = 0; i < listadoStrings.length; i++) {
+				System.out.println(listadoStrings[i]);
+			}
+		}
+		
+		return listadoStrings;
 	}
 	
 }
